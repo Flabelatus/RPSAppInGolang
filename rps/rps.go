c6ff148299b5f3b2816e34815c239921aa58662b
Javid Jooshesh <j.jooshesh@hva.nl>
2023-02-17 16:38:23 +0100

    messages added

--- rps/rps.go
+++ rps/rps.go
@@ -5,6 +5,25 @@ import (
 	"time"
 )
 
+var PlayerWinMsg = []string{
+	"Welldone",
+	"Perfect",
+	"Great Job You Won!",
+	"WOHOOO!",
+}
+var ComputerWinMsg = []string{
+	"Damn!",
+	"Huh! Try Again",
+	"Too Bad!",
+	"...!",
+}
+var DrawWinMsg = []string{
+	"HAha! one more time",
+	"Hmm!",
+	"Ok try again",
+	"Atleast you dont loose",
+}
+
 const (
 	ROCK         = 0 // beats scissors. (scissors + 1) % 3 = 0
 	PAPER        = 1 // beats rock. (rock + 1) % 3 = 1
@@ -18,6 +37,7 @@ type Round struct {
 	Winner         int    `json:"winner"`
 	ComputerChoice string `json:"computer_choice"`
 	RoundResult    string `json:"round_result"`
+	Message        string `json:"message"`
 }
 
 func PlayRound(playerValue int) Round {
@@ -26,6 +46,7 @@ func PlayRound(playerValue int) Round {
 
 	computerChoice := ""
 	roundResult := ""
+	message := ""
 	winner := 0
 
 	switch computerValue {
@@ -41,18 +62,22 @@ func PlayRound(playerValue int) Round {
 	if playerValue == computerValue {
 		roundResult = "It's a draw"
 		winner = DRAW
+		message = DrawWinMsg[rand.Intn(len(DrawWinMsg))]
 	} else if playerValue == (computerValue+1)%3 {
 		roundResult = "Player wins!"
 		winner = PLAYERWINS
+		message = PlayerWinMsg[rand.Intn(len(PlayerWinMsg))]
 	} else {
 		roundResult = "Computer wins!"
 		winner = COMPUTERWINS
+		message = ComputerWinMsg[rand.Intn(len(ComputerWinMsg))]
 	}
 
 	var result Round
 	result.Winner = winner
 	result.ComputerChoice = computerChoice
 	result.RoundResult = roundResult
+	result.Message = message
 
 	return result
 }
